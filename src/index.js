017edf20a223d272bb15ffce3e61ebd2e7d85461
Ne_Plater <dragonsthreeheads@gmail.com>
2022-02-17 17:00:13 +0300

    feat: morse recorder

--- src/index.js
+++ src/index.js
@@ -35,11 +35,42 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+  '' : ' '
 };
 
 function decode(expr) {
-    // write your solution here
-}
+   let data = []; // Your code here
+  for ( let i = 0;  i < expr.length; i=i+10) {
+   data.push(expr.slice(i, i+10));  
+   }
+ 
+  for (let i=0; i<data.length; i++) {
+    if (data[i][0]==='0') {
+      data[i] = String(+data[i]);
+    }
+  }
+  
+  for (let i=0; i<data.length; i++) {
+    let item = '';
+    for (let x=0; x < data[i].length; x=x+2) {
+      
+      if (data[i].slice(x, x+2)==='10') {
+        item = item + '.'
+       }; 
+      if (data[i].slice(x, x+2)==='11') {
+        item = item +'-'
+        };
+      
+    }
+     console.log(item);
+      data[i] = item;
+   }
+  let result = '';
+  for (let i=0; i<data.length; i++) {
+    result = result + MORSE_TABLE[data[i]];
+   }
+  return result;
+};
 
 module.exports = {
     decode
